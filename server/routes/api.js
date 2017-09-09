942dcbf6a819c639049ec00ba9e8ddf5fab297e5
nikrb <nik_rb@yahoo.com>
2017-09-09 19:56:54 +0100

    implement get my books and add book endpoints

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const ol = require( '../modules/OLapi');
+const Book = require( 'mongoose').model( 'Book');
 
 const router = new express.Router();
 
+router.post( '/mybooks', (req, res) => {
+  console.log( "get my books:", req.body);
+  const {email} = req.body;
+  console.log( "get books for owner:", email);
+  Book.find( {'owner.email':email}, function( err, books){
+    if( err || !books || books.length === 0){
+      res.json( {success:false, message: "books not found"});
+    } else {
+      res.json( {success:true, books});
+    }
+  });
+});
+
+router.post( '/book', (req, res) => {
+  console.log( "add book:", req.body);
+  const {owner, title, cover_olid, cover_url_m} = req.body;
+  const book = new Book( {owner, title, cover_olid, cover_url_m});
+  book.save( (err) => {
+    if( err){
+      console.error( "book save failed:", err);
+      res.json( {success: false, message: "book not saved"});
+    } else {
+      res.json( {success: true, message: "book added to your list"});
+    }
+  });
+});
 router.post('/search', (req, res) => {
   const {title} = req.body;
   ol.getBook( title)
@@ -18,7 +45,7 @@ router.post('/search', (req, res) => {
       return ({
         title:d.title_suggest,
         cover_olid: cover_olid,
-        cover_url_s: base_url+"-S.jpg",
+        // cover_url_s: base_url+"-S.jpg",
         cover_url_m: base_url+"-M.jpg"
       });
     });
