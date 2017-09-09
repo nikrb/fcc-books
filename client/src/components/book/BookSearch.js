e09f48a92066de5ffb15ed163b16bea8787a122e
nikrb <nik_rb@yahoo.com>
2017-09-09 18:06:28 +0100

    select text in search box on focus

--- client/src/components/book/BookSearch.js
+++ client/src/components/book/BookSearch.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 export default class BookSearch extends React.Component {
+  onFocus = (e) => {
+    e.target.select();
+  };
   render = () => {
     return (
       <div>
         <input type="text" value={this.props.book_title}
-          onChange={this.props.onBookTitleChange}/>
+          onChange={this.props.onBookTitleChange}
+          onFocus={this.onFocus}/>
         <button type="button" onClick={this.props.onFindBook} >Find</button>
       </div>
     );
