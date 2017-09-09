cd873213680112e25cf079db7f67efe3be7fd741
nikrb <nik_rb@yahoo.com>
2017-09-09 18:08:34 +0100

    fix title text wrap and add no image to missing image placeholder

--- client/src/components/book/BookCover.js
+++ client/src/components/book/BookCover.js
@@ -5,18 +5,25 @@ export default class BookCover extends React.Component {
     this.props.onSelectBook( this.props.data);
   };
   render = () => {
-    console.log( "book cover data:", this.props.data);
     const book = this.props.data;
     const wrapper = {
       display: "flex",
       flexDirection: "column",
+      flexBasis: "170px",
       margin: "5px",
       border: "1px solid lightgrey"
     };
-    const image_url = book.cover_olid?book.cover_url_m:"http://via.placeholder.com/180x180";
+    const text_style = {
+      display:"flex",
+      flexDirection:"row",
+      justifyContent: "center",
+      flexWrap: "wrap"
+    };
+    const image_url = book.cover_olid?book.cover_url_m:"http://via.placeholder.com/180x180?text=noimage";
     return (
       <div style={wrapper}>
         <button type="button" onClick={this.onSelectButton} >select</button>
+        <span style={text_style}>{book.title}</span>
         <img src={image_url} alt='no img' />
       </div>
     );
