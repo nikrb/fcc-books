0a5f9e4c92115beec78e0f56eb4f0ff0748baded
nikrb <nik_rb@yahoo.com>
2017-09-10 00:59:40 +0100

    refactor mongoose book

--- server/models/book.js
+++ server/models/book.js
@@ -1,4 +1,5 @@
 const mongoose = require( 'mongoose');
+const ol = require( '../modules/OLapi');
 
 const BookSchema = new mongoose.Schema({
   cover_olid: String,
@@ -10,5 +11,44 @@ const BookSchema = new mongoose.Schema({
   title: String,
   cover_url_m: String
 });
+
+BookSchema.statics.getMyBooks = function getMyBooks( req_body, cb){
+  console.log( "get my books:", req_body);
+  const {email} = req_body;
+  console.log( "get books for owner:", email);
+  this.find( {'owner.email':email}, function( err, books){
+    if( err || !books || books.length === 0){
+      if( cb) cb( err, {success:false, message: "books not found"});
+    } else {
+      if( cb) cb( null, { success:true, books});
+    }
+  });
+}
+BookSchema.statics.search = function search( req_body, cb){
+  const {title} = req_body;
+  ol.getBook( title)
+  .then( (response) => {
+    const data = response.docs.map( (d) => {
+      let cover_olid = "noimage";
+      let base_url = ol.getCoverUrl( d.cover_edition_key);
+      if( base_url) cover_olid = d.cover_edition_key;
+      if( typeof base_url === 'undefined'){
+        base_url = ol.getCoverUrl( d.edition_key);
+        if( base_url) cover_olid = d.edition_key;
+      }
+      return ({
+        title:d.title_suggest,
+        cover_olid: cover_olid,
+        // cover_url_s: base_url+"-S.jpg",
+        cover_url_m: base_url+"-M.jpg"
+      });
+    });
+    if( cb) cb( null, data);
+  })
+  .catch( (err) => {
+    console.error( "ol book search failed:", err);
+    if( cb) cb( err, {success: false, message: "books not found"});
+  });
+}
 
 module.exports = mongoose.model('Book', BookSchema);
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,21 +1,7 @@
 const express = require('express');
-const ol = require( '../modules/OLapi');
 const Book = require( 'mongoose').model( 'Book');
 
 const router = new express.Router();
-
-router.post( '/mybooks', (req, res) => {
-  console.log( "get my books:", req.body);
-  const {email} = req.body;
-  console.log( "get books for owner:", email);
-  Book.find( {'owner.email':email}, function( err, books){
-    if( err || !books || books.length === 0){
-      res.json( {success:false, message: "books not found"});
-    } else {
-      res.json( {success:true, books});
-    }
-  });
-});
 
 router.post( '/book', (req, res) => {
   console.log( "add book:", req.body);
@@ -30,29 +16,22 @@ router.post( '/book', (req, res) => {
     }
   });
 });
+
+router.post( '/mybooks', (req, res) => {
+  Book.getMyBooks( req.body, function( err, docs){
+    if( err || docs.length === 0){
+      console.error( "find my books failed:", err);
+    }
+    res.json( docs);
+  });
+});
+
 router.post('/search', (req, res) => {
-  const {title} = req.body;
-  ol.getBook( title)
-  .then( (response) => {
-    const data = response.docs.map( (d) => {
-      let cover_olid = "noimage";
-      let base_url = ol.getCoverUrl( d.cover_edition_key);
-      if( base_url) cover_olid = d.cover_edition_key;
-      if( typeof base_url === 'undefined'){
-        base_url = ol.getCoverUrl( d.edition_key);
-        if( base_url) cover_olid = d.edition_key;
-      }
-      return ({
-        title:d.title_suggest,
-        cover_olid: cover_olid,
-        // cover_url_s: base_url+"-S.jpg",
-        cover_url_m: base_url+"-M.jpg"
-      });
-    });
-    res.json( data);
-  })
-  .catch( (err) => {
-    console.error( "ol book search failed:", err);
+  Book.search( req.body, function( err, docs){
+    if( err || docs.length === 0){
+      console.error( "book search failed:", err);
+    }
+    res.json( docs);
   });
 });
 
