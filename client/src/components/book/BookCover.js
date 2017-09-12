11e79d9fd5398a1014045d0102afa4cdf2408e9c
nikrb <nik_rb@yahoo.com>
2017-09-12 09:56:40 +0100

    check there is a book owner before displaying

--- client/src/components/book/BookCover.js
+++ client/src/components/book/BookCover.js
@@ -17,10 +17,13 @@ export default class BookCover extends React.Component {
       flexWrap: "wrap"
     };
     const image_url = book.cover_olid?book.cover_url_m:"http://via.placeholder.com/180x180?text=noimage";
+    // started book.owner.name for testing, but there is no owner for open library
+    // book search
     return (
       <div style={wrapper}>
         {this.props.children}
         <span style={text_style}>{book.title}</span>
+        {book.owner?<span style={text_style}>{book.owner.name}</span>:null}
         <img src={image_url} alt='no img' />
       </div>
     );
