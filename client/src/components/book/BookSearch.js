e62b90655df1d0df98adb1a64cfc3ab3650f0ae0
nikrb <nik_rb@yahoo.com>
2017-09-13 17:30:49 +0100

    tweak placeholder text for book title and author search fields

--- client/src/components/book/BookSearch.js
+++ client/src/components/book/BookSearch.js
@@ -7,12 +7,14 @@ export default class BookSearch extends React.Component {
   render = () => {
     return (
       <div>
+        Title&nbsp;
         <input type="text" value={this.props.book_title}
-          name="book_title" placeholder="Book Title ..."
+          name="book_title" placeholder="Search text ..."
           onChange={this.props.onBookChange}
           onFocus={this.onFocus} />
+        Author&nbsp;
         <input type="text" value={this.props.book_author}
-          name="book_author" placeholder="Author"
+          name="book_author" placeholder="Search text ..."
           onChange={this.props.onBookChange}
           onFocus={this.onFocus} />
         <button type="button" onClick={this.props.onFindBook}
